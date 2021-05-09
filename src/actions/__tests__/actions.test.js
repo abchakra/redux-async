5282f950362f0f45036e4ded06a22083664ed6d9
Abhishek Chakraborty <a.chakraborty26@gmail.com>
2021-05-09 07:03:13 +0200

    test the async action creators

---
+++ src/actions/__tests__/actions.test.js
@@ -0,0 +1,33 @@
+import * as actions from '../index'
+
+describe('actions', () => {
+         const subreddit = 'reactjs'
+
+         // Add the mockJSON response
+         const mockJSON = {
+                  data: {
+                           children: [{ data: { title: "Post 1" } }, { data: { title: "Post 2" } }]
+                  }
+         };
+
+         describe('selectSubreddit', () => {
+                  it('should create an action with a given subreddit', () => {
+                           const expectedAction = {
+                                    type: actions.SELECT_SUBREDDIT,
+                                    subreddit
+                           }
+                           expect(actions.selectSubreddit(subreddit)).toEqual(expectedAction)
+                  })
+         })
+
+         describe('receivePosts', () => {
+                  it('should create the expected action', () => {
+                           const expectedAction = {
+                                    type: actions.RECEIVE_POSTS,
+                                    subreddit,
+                                    posts: actions.transformResponseBody(mockJSON),
+                           }
+                           expect(actions.receivePosts(subreddit, mockJSON)).toMatchObject(expectedAction);
+                  })
+         })
+})
